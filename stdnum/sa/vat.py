84b7ff95c9c738502c0a329b8bd2636ac215e35e
ahmed <ahmed.boori@gmail.com>
2022-01-15 20:29:38 +0000

    add Saudi Numbers

---
+++ stdnum/sa/vat.py
@@ -0,0 +1,48 @@
+# vat.py - functions for handling Saudi Arabian VAT numbers
+# coding: utf-8
+#
+# Copyright (C) 2021 Ahmed AlBoori
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+from stdnum.exceptions import *
+from stdnum.util import clean, isdigits
+
+def compact(number):
+    """Convert the number to the minimal representation. This strips the
+    number of any valid separators and removes surrounding whitespace."""
+    
+    number = clean(number, ' -./:').upper().strip()
+    return number
+
+def validate(number):
+    """Check if the number is a valid VAT number. This checks the length,
+    formatting and check digit."""
+    number = compact(number)
+    if not isdigits(number):
+        raise InvalidFormat()
+    if len(number) != 15:
+        raise InvalidLength()
+
+    return number
+
+
+def is_valid(number):
+    """Check if the number is a valid VAT number."""
+    try:
+        return bool(validate(number))
+    except ValidationError:
+        return False
